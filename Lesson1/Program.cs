d2cebc7365fb25f3bc27988897b3aa456cc0f15a
anait <anaitgrig@mail.ru>
2022-10-06 03:23:26 +0300

    put */

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
+/*
 Console.Write("Input a first number: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
@@ -13,10 +13,10 @@ else
 {
     Console.Write("The max number is " + n2);
 }
-
+*/
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
+/*
 Console.Write("Input a first number: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
@@ -40,10 +40,10 @@ if(n3 > max)
     max = n3;
 }
     Console.Write("The max number is " + max);
-
+*/
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-
+/*
 Console.Write("Input a number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
@@ -55,10 +55,10 @@ else
 {
     Console.WriteLine("Odd number");
 }
-
+*/
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
+/*
 Console.Write("Input a number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
@@ -71,7 +71,7 @@ while(current <= n)
     Console.Write(current + " ");
     current++;
 }
-
+*/
 
 
     
